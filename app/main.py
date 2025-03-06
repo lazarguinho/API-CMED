52204742e17d8236a26d976c6c033ca76c5dab39
Mateus Pedrosa <mpedrosa001@gmail.com>
2025-03-06 02:58:49 -0300

    Add rotas no main

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
-from app.routes import medicamento
-from app.config.database import get_database  # Agora importamos de config/database.py
+from app.routes import medicamento, laboratorio, substancia, registro, historico_preco
 
 app = FastAPI(title="API-CMED - Medicamentos")
 
-# Incluir rotas da API
 app.include_router(medicamento.router, prefix="/api", tags=["Medicamentos"])
+app.include_router(laboratorio.router, prefix="/api", tags=["Laboratorios"])
+app.include_router(substancia.router, prefix="/api", tags=["Substancias"])
+app.include_router(registro.router, prefix="/api", tags=["Registros"])
+app.include_router(historico_preco.router, prefix="/api", tags=["HistoricoPrecos"])
